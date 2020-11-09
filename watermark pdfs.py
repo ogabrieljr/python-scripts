95cc6ab31eff8b58c168c0ead47bf7c24caf6bac
junior <jgj_junior@yahoo.com.br>
2020-11-09 20:37:00 -0300

    pdf mani and iris ml

---
+++ watermark pdfs.py
@@ -0,0 +1,21 @@
+import PyPDF2
+import os
+
+watermark = PyPDF2.PdfFileReader("watermark.pdf")
+output = PyPDF2.PdfFileWriter()
+merger = PyPDF2.PdfFileMerger()
+
+if os.path.exists("output.pdf"):
+    template = PyPDF2.PdfFileReader("output.pdf")
+    for i in range(template.getNumPages()):
+        page = template.getPage(i)
+        page.mergePage(watermark.getPage(0))
+        output.addPage(page)
+        output.write(open("watermarked.pdf", "wb"))
+        
+else:
+    def pdf_combiner(pdf_list):
+        for pdf in pdf_list:
+            merger.append(pdf)
+        merger.write("output.pdf")
+    pdf_combiner(["pdf1.pdf", "pdf2.pdf"])
